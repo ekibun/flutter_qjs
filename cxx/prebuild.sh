8b279c29266a10574772384586f5dea18a08ad48
ekibun <soekibun@gmail.com>
2021-10-03 22:09:08 +0800

    coverage

--- cxx/prebuild.sh
+++ cxx/prebuild.sh
@@ -7,27 +7,11 @@ mkdir ./cxx
 sed 's/\#include \"quickjs\/quickjs.h\"/\#include \"quickjs.h\"/g' ../cxx/ffi.h > ./cxx/ffi.h
 cp ../cxx/ffi.cpp ./cxx/ffi.cpp
 
+cp ../cxx/quickjs/*.h ./cxx/
+cp ../cxx/quickjs/*.c ./cxx/
+
 quickjs_version=$(cat ../cxx/quickjs/VERSION)
 
 sed '1i\
 \#define CONFIG_VERSION \"'$quickjs_version'\"\
 ' ../cxx/quickjs/quickjs.c > ./cxx/quickjs.c
-
-quickjs_src=(
-    "list.h"
-    "cutils.c"
-    "libregexp.c"
-    "libunicode.c"
-    "cutils.h"
-    "libregexp.h"
-    "libunicode.h"
-    "quickjs.h"
-    "quickjs-atom.h"
-    "quickjs-opcode.h"
-    "libregexp-opcode.h"
-    "libunicode-table.h"
-)
-for item in ${quickjs_src[*]}
-do
-    cp ../cxx/quickjs/$item ./cxx/$item
-done
